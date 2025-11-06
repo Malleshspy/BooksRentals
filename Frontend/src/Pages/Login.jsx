4702783e33e21486a45097d56b3c5c8c94811a00
Somesh2504 <someshchevula25@gmail.com>
2025-11-06 11:24:10 +0530

    login booksss

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,65 +1,3 @@
-// import { useState } from "react";
-// import axios from "axios";
-
-// const Login = () => {
-//   const [form, setForm] = useState({ email: "", password: "" });
-//   const [message, setMessage] = useState("");
-
-//   const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const res = await axios.post("http://localhost:3000/api/users/login", form);
-//       setMessage(res.data.message);
-//     } catch (err) {
-//       setMessage(err.response?.data?.message || "Login failed");
-//     }
-//   };
-
-//   return (
-//     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-600 to-purple-600 px-4">
-//       <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md">
-//         <h2 className="text-3xl font-bold text-center text-indigo-600 mb-6">
-//           Login
-//         </h2>
-//         <form onSubmit={handleSubmit} className="space-y-5">
-//           <div>
-//             <label className="block text-gray-700 mb-2">Email</label>
-//             <input
-//               type="email"
-//               name="email"
-//               value={form.email}
-//               onChange={handleChange}
-//               required
-//               className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-indigo-500 outline-none"
-//             />
-//           </div>
-//           <div>
-//             <label className="block text-gray-700 mb-2">Password</label>
-//             <input
-//               type="password"
-//               name="password"
-//               value={form.password}
-//               onChange={handleChange}
-//               required
-//               className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-indigo-500 outline-none"
-//             />
-//           </div>
-//           <button
-//             type="submit"
-//             className="w-full bg-indigo-600 text-white py-2 rounded-lg hover:bg-indigo-700 transition"
-//           >
-//             Login
-//           </button>
-//         </form>
-//         {message && <p className="text-center mt-4 text-indigo-700 font-semibold">{message}</p>}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Login;
 import { useState } from "react";
 import axios from "axios";
 import { useEffect } from "react";
@@ -182,3 +120,4 @@ const Login = () => {
 
 export default Login;
 
+//kkk
